70a799b39c54af7f7a289a85c5555ae31a390e6d
Will Hartmann <will.hartmann@vtpventures.com>
2017-01-04 16:37:27 -0800

    [#33] Created SocrataStringArgument.php argument plugin

---
+++ socrata_views/src/Plugin/views/argument/SocrataStringArgument.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\socrata_views\Plugin\views\argument\StringArgument.
+ */
+
+namespace Drupal\socrata_views\Plugin\views\argument;
+
+use Drupal\views\Plugin\views\argument\StringArgument;
+
+/**
+ * Basic textfield argument to handle string arguments.
+ *
+ * @ingroup views_argument_handlers
+ *
+ * @ViewsArgument("socrata_string")
+ */
+class SocrataStringArgument extends StringArgument {
+  /**
+   * Add this argument to the query.
+   *
+   * Due to the nature of fapi, the value and the operator have an unintended
+   * level of indirection. You will find them in $this->operator
+   * and $this->value respectively.
+   */
+  public function query($group_by = FALSE) {
+    $this->query->addWhere(0, $this->realField, $this->argument, '=');
+  }
+
+}
