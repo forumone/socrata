7c29a37a852898bb1ff103b4207ed41310b0651e
Will Hartmann <will.hartmann@vtpventures.com>
2017-02-10 15:59:50 -0800

    [#34] Cleaned up method comments

--- socrata_catalog_search/src/Plugin/views/filter/SocrataFullText.php
+++ socrata_catalog_search/src/Plugin/views/filter/SocrataFullText.php
@@ -11,9 +11,6 @@ use Drupal\views\Plugin\views\filter\StringFilter;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Basic textfield filter to handle string filtering commands
- * including equality, like, not like, etc.
- *
  * @ingroup views_filter_handlers
  *
  * @ViewsFilter("socrata_full_text")
