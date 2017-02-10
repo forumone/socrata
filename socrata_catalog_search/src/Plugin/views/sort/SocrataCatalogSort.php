fc329e075ccf1eb0e9979f2d5e96e0c149f0b333
Will Hartmann <will.hartmann@vtpventures.com>
2017-02-10 13:20:58 -0800

    [#34] Created SocrataCatalogSort.php

---
+++ socrata_catalog_search/src/Plugin/views/sort/SocrataCatalogSort.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\socrata_catalog_search\Plugin\views\sort\SocrataCatalogSort.
+ */
+
+namespace Drupal\socrata_catalog_search\Plugin\views\sort;
+
+use Drupal\views\Plugin\views\sort\SortPluginBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Default implementation of the base sort plugin.
+ *
+ * @ingroup views_sort_handlers
+ *
+ * @ViewsSort("socrata_catalog_sort")
+ */
+class SocrataCatalogSort extends SortPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    parent::buildOptionsForm($form, $form_state);
+    $form['order']['#description'] = t('Note that because of limitations in the Socrata Catalog Search API, sorting only works on the visible paged results and not the entire data set.');
+  }
+
+}
