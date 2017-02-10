e1ec66911852dc6e4fc47bc0111815641bdc9791
Will Hartmann <will.hartmann@vtpventures.com>
2017-02-10 15:59:02 -0800

    [#34] Created SocrataType.php

---
+++ socrata_catalog_search/src/Plugin/views/filter/SocrataType.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\socrata_catalog_search\Plugin\views\filter\SocrataType.
+ */
+
+namespace Drupal\socrata_catalog_search\Plugin\views\filter;
+
+use Drupal\views\Plugin\views\filter\InOperator;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * @ingroup views_filter_handlers
+ *
+ * @ViewsFilter("socrata_type")
+ */
+class SocrataType extends InOperator {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getValueOptions() {
+   $this->valueOptions = [
+      'charts' => $this->t("Charts"),
+      'datalenses' => $this->t("Datalenses"),
+      'datasets' => $this->t("Datasets"),
+      'files' => $this->t("Files"),
+      'hrefs' => $this->t("Hrefs"),
+      'maps' => $this->t("Maps"),
+      'stories' => $this->t("Stories"),
+    ];
+    return $this->valueOptions;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  function operators() {
+    $operators = parent::operators();
+
+    // Disable unsupported operator.
+    unset($operators['not in']);
+
+    return $operators;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function opSimple() {
+    if (empty($this->value)) {
+      return;
+    }
+    $this->ensureMyTable();
+
+    // We use array_values() because the checkboxes keep keys and that can cause
+    // array addition problems.
+    // Override field.
+    $this->query->addWhere($this->options['group'], "only", array_values($this->value), $this->operator);
+  }
+
+}
