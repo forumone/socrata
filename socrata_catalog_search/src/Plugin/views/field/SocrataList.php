6f6f68cb1d7ff27b9d02ba9ff76fc849465c80ac
Will Hartmann <will.hartmann@vtpventures.com>
2017-02-10 11:29:14 -0800

    [#34] Created SocrataList.php field plugin

---
+++ socrata_catalog_search/src/Plugin/views/field/SocrataList.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\socrata_catalog_search\Plugin\views\field\SocrataList.
+ * Based heavily on date with the only major difference
+ * being that Socrata stores this field type in ISO 8601 format.
+ */
+
+namespace Drupal\socrata_catalog_search\Plugin\views\field;
+
+use Drupal\views\ResultRow;
+use Drupal\views\Plugin\views\field\PrerenderList;
+
+/**
+ * A handler to provide proper displays for calendar dates.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("socrata_list")
+ */
+class SocrataList extends PrerenderList {
+
+  /**
+   * {@inheritdoc}
+   *
+   * Items should be stored in the result array, if possible, as an array
+   * with 'value' as the actual displayable value of the item, plus
+   * any items that might be found in the 'alter' options array for
+   * creating links, such as 'path', 'fragment', 'query' etc, such a thing
+   * is to be made. Additionally, items that might be turned into tokens
+   * should also be in this array.
+   */
+  public function getItems(ResultRow $values) {
+    $field = $this->getValue($values);
+
+    $items = explode(',', $field);
+    if (!empty($items)) {
+      $this->items = [];
+      foreach ($items as $count => $value) {
+        // Array needs to have a themeable keyed value.
+        $this->items[$count] = ['#plain_text' => $value];
+      }
+  
+      return $this->items;
+    }
+
+    return [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render_item($count, $item) {
+    // Return the bare value so that the string isn't double-escaped.
+    return $item['#plain_text'];
+  }
+}
