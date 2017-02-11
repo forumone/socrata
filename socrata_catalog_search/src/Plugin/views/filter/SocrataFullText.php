a9b4ce073df3f744ccea94600745d6d00aad2d29
Will Hartmann <will.hartmann@vtpventures.com>
2017-02-10 16:23:06 -0800

    [#34] Changed concrete classes to use abstract class

--- socrata_catalog_search/src/Plugin/views/filter/SocrataFullText.php
+++ socrata_catalog_search/src/Plugin/views/filter/SocrataFullText.php
@@ -7,58 +7,15 @@
 
 namespace Drupal\socrata_catalog_search\Plugin\views\filter;
 
-use Drupal\views\Plugin\views\filter\StringFilter;
-use Drupal\Core\Form\FormStateInterface;
+use Drupal\socrata_catalog_search\Plugin\views\filter\SocrataFilter;
 
 /**
  * @ingroup views_filter_handlers
  *
  * @ViewsFilter("socrata_full_text")
  */
-class SocrataFullText extends StringFilter {
+class SocrataFullText extends SocrataFilter {
 
-  /**
-   * {@inheritdoc}
-   */
-  function operators() {
-    $operators = array(
-      '=' => array(
-        'title' => t('Contains'),
-        'method' => 'opEqual',
-        'short' => t('='),
-        'values' => 1,
-      ),
-    );
-
-    return $operators;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function valueForm(&$form, FormStateInterface $form_state) {
-    parent::valueForm($form, $form_state);
-
-    // Add help text.
-    $form['value']['#description'] = t('You may enter multiple values separated by a comma as "OR" query parameters.');
-  }
-
-  /**
-   * Add this filter to the query.
-   *
-   * Due to the nature of fapi, the value and the operator have an unintended
-   * level of indirection. You will find them in $this->operator
-   * and $this->value respectively.
-   */
-  public function query() {
-    $info = $this->operators();
-
-    // Override the field name per http://labs.socrata.com/docs/search.html#-full-text-search.
-    $field = 'q';
-
-    if (!empty($info[$this->operator]['method'])) {
-      $this->{$info[$this->operator]['method']}($field);
-    }
-  }
+  public $search_field = 'q';
 
 }
